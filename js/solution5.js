0d9be80ee62bce30d9ebd9518a9f656245b6682b
MateuszZaniewski <mateusz.zaniewski94@gmail.com>
2022-06-16 17:24:06 +0200

    Solution 5

---
+++ js/solution5.js
@@ -0,0 +1,32 @@
+// Task
+// Sum all the numbers of a given array ( cq. list ), except the highest and the lowest element ( by value, not by index! ).
+
+// The highest or lowest element respectively is a single element at each edge, even if there are more than one with the same value.
+
+// Mind the input validation.
+
+// Example
+// { 6, 2, 1, 8, 10 } => 16
+// { 1, 1, 11, 2, 3 } => 6
+// Input validation
+// If an empty value ( null, None, Nothing etc. ) is given instead of an array, or the given array is an empty list or a list with only 1 element, return 0.
+
+/*
+    My Solution :
+    1. Check if array equals null/undefined or contains only single value, if so return 0
+    2. Check for min and max values
+    3. Reduce array to single value and substract min and max and return result
+
+*/
+
+
+function sumArray(array) {
+    if(array === null || array === undefined || array.length <= 1) {
+        return 0
+    }
+    let min = Math.min(...array)
+    let max = Math.max(...array)
+    return (array.reduce((a,b) => a + b))-min-max
+}
+
+console.log(sumArray([null]))
