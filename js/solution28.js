0769596eb9e05c544c78488aab9dc6ed38f168a6
MateuszZaniewski <mateusz.zaniewski94@gmail.com>
2022-09-24 10:51:39 +0200

    [7 kyu] Ones and Zeros

---
+++ js/solution28.js
@@ -0,0 +1,28 @@
+// Given an array of ones and zeroes, convert the equivalent binary value to an integer.
+
+// Eg: [0, 0, 0, 1] is treated as 0001 which is the binary representation of 1.
+
+// Examples:
+
+// Testing: [0, 0, 0, 1] ==> 1
+// Testing: [0, 0, 1, 0] ==> 2
+// Testing: [0, 1, 0, 1] ==> 5
+// Testing: [1, 0, 0, 1] ==> 9
+// Testing: [0, 0, 1, 0] ==> 2
+// Testing: [0, 1, 1, 0] ==> 6
+// Testing: [1, 1, 1, 1] ==> 15
+// Testing: [1, 0, 1, 1] ==> 11
+// However, the arrays can have varying lengths, not just limited to 4.
+
+/*
+    1. Join array using join method
+    2. Use ParseInt method to convert given string to number
+*/
+
+const binaryArrayToNumber = arr => {
+    let x = (arr.join(''))
+    console.log(x)
+    return parseInt(x, 2)
+  }
+
+console.log(binaryArrayToNumber([0, 0, 0, 1, 1]))
