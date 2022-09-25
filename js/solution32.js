8d944fc2f1a40bd5e220875f2c847a729a94d08d
MateuszZaniewski <mateusz.zaniewski94@gmail.com>
2022-09-25 15:04:31 +0200

    [8 kyu] Sort and Star

---
+++ js/solution32.js
@@ -0,0 +1,12 @@
+// You will be given a list of strings. You must sort it alphabetically (case-sensitive, and based on the ASCII values of the chars) and then return the first value.
+
+// The returned value must be a string, and have "***" between each of its letters.
+
+// You should not remove or add elements from/to the array.
+
+function twoSort(s) {
+    let sorted = s.sort()
+    return sorted[0].split('').join('***')
+}
+
+console.log(twoSort(["turns", "out", "random", "test", "cases", "are", "easier", "than", "writing", "out", "basic", "ones"]))
