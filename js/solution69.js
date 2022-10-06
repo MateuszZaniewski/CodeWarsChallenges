7281dd8d15b663b5803b86e0950c87626e615771
MateuszZaniewski <mateusz.zaniewski94@gmail.com>
2022-10-06 15:30:14 +0200

    [8 kyu] 5 without numbers !!

--- js/solution69.js
+++ js/solution69.js
@@ -1,15 +1,13 @@
 // DESCRIPTION:
-// This kata is from check py.checkio.org
+// Write a function that always returns 5
 
-// You are given an array with positive numbers and a non-negative number N. You should find the N-th power of the element in the array with the index N. If N is outside of the array, then return -1. Don't forget that the first element has the index 0.
+// Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
 
-// Let's look at a few examples:
+// Good luck :)
 
-// array = [1, 2, 3, 4] and N = 2, then the result is 3^2 == 9;
-// array = [1, 2, 3] and N = 3, but N is outside of the array, so the result is -1.
 
-function index(array, n){
-    return n < array.length ? Math.pow(array[n], n) : -1
-   }
+function unusualFive() {
+    return 'putin'.length
+  }
 
-console.log(index([1, 2, 3, 4],2))
+  console.log(unusualFive())
