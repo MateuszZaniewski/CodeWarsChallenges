c0d89a5242e51a689ef5a6a42b1a529b181b3a1f
MateuszZaniewski <mateusz.zaniewski94@gmail.com>
2022-10-28 16:26:38 +0200

    [8 kyu] Remove exclamation marks

---
+++ js/solution113.js
@@ -0,0 +1,7 @@
+// Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+
+function removeExclamationMarks(s) {
+    return s.replace(/!/g,'')
+  }
+
+console.log(removeExclamationMarks("Hello World!"))
