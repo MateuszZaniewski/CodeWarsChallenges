76a1ba7d3da32611e92e720fb80bfc1dc92825d0
MateuszZaniewski <mateusz.zaniewski94@gmail.com>
2022-11-07 07:30:10 +0100

    [7 kyu] Squares sequence

---
+++ js/solution147.js
@@ -0,0 +1,19 @@
+// Complete the function that returns an array of length n, starting with the given number x and the squares of the previous number. If n is negative or zero, return an empty array/list.
+
+// Examples
+// 2, 5  -->  [2, 4, 16, 256, 65536]
+// 3, 3  -->  [3, 9, 81]
+
+
+function squares(x, n) {
+    let newArr = []
+    let result = x
+    for(let i = 0; i < n ; i++){
+            newArr.push(result)
+            result = x * x
+            x = result
+    }
+    return newArr
+}
+
+console.log(squares(2,5))
