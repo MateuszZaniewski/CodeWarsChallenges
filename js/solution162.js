e6367ec57abb20f6cb8226ae552ce17fc5d0813f
MateuszZaniewski <mateusz.zaniewski94@gmail.com>
2022-11-12 10:11:57 +0100

    [8 kyu] Hex to Decimal

---
+++ js/solution162.js
@@ -0,0 +1,9 @@
+// Complete the function which converts hex number (given as a string) to a decimal number.
+
+
+function hexToDec(hexString){
+    //your code here
+    return parseInt(hexString, 16)
+  }
+
+console.log(hexToDec('a'))
