75fae123fc04ff4f088d250e1ba37c3bef1cf60d
MateuszZaniewski <mateusz.zaniewski94@gmail.com>
2022-11-12 10:17:11 +0100

    [8 kyu] Return the day

---
+++ js/solution163.js
@@ -0,0 +1,24 @@
+// Complete the function which returns the weekday according to the input number:
+
+// 1 returns "Sunday"
+// 2 returns "Monday"
+// 3 returns "Tuesday"
+// 4 returns "Wednesday"
+// 5 returns "Thursday"
+// 6 returns "Friday"
+// 7 returns "Saturday"
+// Otherwise returns "Wrong, please enter a number between 1 and 7"
+
+function whatday(num) { 
+    // put your code here
+    return num == 1 ? 'Sunday' :
+           num == 2 ? 'Monday' :
+           num == 3 ? 'Tuesday':
+           num == 4 ? 'Wednesday':
+           num == 5 ? 'Thursday' :
+           num == 6 ? 'Friday' :
+           num == 7 ? 'Saturday' :
+           'Wrong, please enter a number between 1 and 7'
+  }
+
+console.log(whatday(3))
