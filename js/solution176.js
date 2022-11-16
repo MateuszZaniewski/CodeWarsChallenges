e6c7f3c7c3e140fa721090f373cc27c7924f8ce5
MateuszZaniewski <mateusz.zaniewski94@gmail.com>
2022-11-16 15:42:20 +0100

    [8 kyu] Formatting decimal places #0

---
+++ js/solution176.js
@@ -0,0 +1,12 @@
+// Each number should be formatted that it is rounded to two decimal places. You don't need to check whether the input is a valid number because only valid numbers are used in the tests.
+
+// Example:    
+// 5.5589 is rounded 5.56   
+// 3.3424 is rounded 3.34
+
+function twoDecimalPlaces(n) {
+    // Your code here
+    return Number(n.toFixed(2))
+  }
+
+console.log(twoDecimalPlaces(4.659725356))
