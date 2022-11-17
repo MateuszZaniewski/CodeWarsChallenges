4f81f102a6b16fdf850f5d66257e9ab5ec49fc3a
MateuszZaniewski <mateusz.zaniewski94@gmail.com>
2022-11-17 18:01:17 +0100

    [7 kyu] Minimize Sum Of Array (Array Series #1)

---
+++ js/solution179.js
@@ -0,0 +1,32 @@
+// Task
+// Given an array of integers , Find the minimum sum which is obtained from summing each Two integers product .
+
+// Notes
+// Array/list will contain positives only .
+// Array/list will always have even size
+// Input >> Output Examples
+// minSum({5,4,2,3}) ==> return (22) 
+// Explanation:
+// The minimum sum obtained from summing each two integers product ,  5*2 + 3*4 = 22
+// minSum({12,6,10,26,3,24}) ==> return (342)
+// Explanation:
+// The minimum sum obtained from summing each two integers product ,  26*3 + 24*6 + 12*10 = 342
+// minSum({9,2,8,7,5,4,0,6}) ==> return (74)
+// Explanation:
+// The minimum sum obtained from summing each two integers product ,  9*0 + 8*2 +7*4 +6*5 = 74
+
+
+function minSum(arr) {
+    // your code here
+    let result = 0
+    arr.sort((a,b) => a - b)
+    for(let i = 0; i <= arr.length*100; i++){
+        result += arr[0]*arr[arr.length-1]
+        arr.splice(0,1)
+        arr.splice(-1)
+        console.log(arr.length)
+    }
+    return result
+  }
+
+console.log(minSum([12,6,10,26,3,24]))
