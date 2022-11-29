2704f771a6eb51a958cc8bc510094a75ebb731c8
MateuszZaniewski <mateusz.zaniewski94@gmail.com>
2022-11-29 15:53:57 +0100

    [7 kyu] How many arguments

---
+++ js/solution220.js
@@ -0,0 +1,13 @@
+// How many arguments
+
+// args_count(1, 2, 3) -> 3
+// args_count(1, 2, 3, 10) -> 4
+
+// Create a function called args_count that returns the number of arguments provided
+
+
+function args_count(...arguments){
+    return arguments.length
+}
+
+console.log(args_count(1,2,3,4))
