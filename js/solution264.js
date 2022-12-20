85dc0497c74b840a215626cef2c128ba70cc89e1
MateuszZaniewski <mateusz.zaniewski94@gmail.com>
2022-12-20 08:57:14 +0100

    [7 kyu] max diff - easy

---
+++ js/solution264.js
@@ -0,0 +1,14 @@
+// DESCRIPTION:
+// You must implement a function that returns the difference between the largest and the smallest value in a given list / array (lst) received as the parameter.
+
+// lst contains integers, that means it may contain some negative numbers
+// if lst is empty or contains a single element, return 0
+// lst is not sorted
+// [1, 2, 3, 4]   //  returns 3 because 4 -   1  == 3
+// [1, 2, 3, -4]  //  returns 7 because 3 - (-4) == 7
+
+function maxDiff(list) {
+    return list.length > 0 ? Math.max(...list) - Math.min(...list) : 0
+  };
+
+console.log(maxDiff([-0, 1, 2, -3, 4, 5, -6]))
