0bbad6b8a9a0ba1d71023a7beb6b448b5ebdbbf3
MateuszZaniewski <mateusz.zaniewski94@gmail.com>
2023-02-06 18:32:44 +0100

    [7 kyu] Longest vowel chain

---
+++ js/solution340.js
@@ -0,0 +1,10 @@
+// The vowel substrings in the word codewarriors are o,e,a,io. The longest of these has a length of 2. Given a lowercase string that has alphabetic characters only (both vowels and consonants) and no spaces, return the length of the longest vowel substring. Vowels are any of aeiou.
+
+// Good luck!
+
+function solve(s){
+    let solve = s.split(/[qwrtypsdfghjklzxcvbnm]+/)
+    return Math.max(...(solve.map(el => el.length)))
+   }
+
+console.log(solve("codewarriors"))
