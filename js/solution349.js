5306a818a9f0e2b08c5cce784650e0b59bb3f805
MateuszZaniewski <mateusz.zaniewski94@gmail.com>
2023-02-11 12:35:20 +0100

    [6 kyu] English beggars

---
+++ js/solution349.js
@@ -0,0 +1,44 @@
+// DESCRIPTION:
+// Born a misinterpretation of this kata, your task here is pretty simple: given an array of values and an amount of beggars, you are supposed to return an array with the sum of what each beggar brings home, assuming they all take regular turns, from the first to the last.
+
+// For example: [1,2,3,4,5] for 2 beggars will return a result of [9,6], as the first one takes [1,3,5], the second collects [2,4].
+
+// The same array with 3 beggars would have in turn have produced a better out come for the second beggar: [5,7,3], as they will respectively take [1,4], [2,5] and [3].
+
+// Also note that not all beggars have to take the same amount of "offers", meaning that the length of the array is not necessarily a multiple of n; length can be even shorter, in which case the last beggars will of course take nothing (0).
+
+// Note: in case you don't get why this kata is about English beggars, then you are not familiar on how religiously queues are taken in the kingdom ;)
+
+// Note 2: do not modify the input array.
+
+function beggars(values, n){
+    if(n == 1) {
+        return [values.reduce((a,b) => a + b)]
+    }
+    else if(n == 0){
+        return []
+    }
+    else if(n <= values.length) {
+        let arr = [...values]
+        let count = 0
+        for(let i = n; i < values.length; i++){
+            if(count >= n) { count = 0}
+            arr[count] += values[i]
+            count++
+        }
+        return (arr.slice(0,n))
+    }
+    else {
+        let arr = [...values]
+        let diff = n - arr.length
+        for(let i = 0; i < diff; i++){
+            arr.push(0)
+        }
+        return arr
+    }
+  }
+
+console.log(beggars([1,2,3,4,5],2))
+
+//[5, 6, 3]
+//[5, 7, 3]
