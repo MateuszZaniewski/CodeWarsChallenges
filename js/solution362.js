89fade8540a6171676b832557bc95205184b04cf
MateuszZaniewski <mateusz.zaniewski94@gmail.com>
2023-02-22 17:41:55 +0100

    [7 kyu] The Office IV - Find a Meeting Room

---
+++ js/solution362.js
@@ -0,0 +1,14 @@
+// Your job at E-Corp is both boring and difficult. It isn't made any easier by the fact that everyone constantly wants to have a meeting with you, and that the meeting rooms are always taken!
+
+// In this kata, you will be given an array. Each value represents a meeting room. Your job? Find the first empty one and return its index (N.B. There may be more than one empty room in some test cases).
+
+// 'X' --> busy
+// 'O' --> empty
+// If all rooms are busy, return "None available!"
+
+
+function meeting(x){
+    return x.includes('O') ? x.indexOf('O') : "None available!"
+}
+
+console.log(meeting(['X', 'O', 'X']))
