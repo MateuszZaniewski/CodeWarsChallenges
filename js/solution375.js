73c6dbed9f74b8cd88009ddf81ef6b6389a559ae
MateuszZaniewski <mateusz.zaniewski94@gmail.com>
2023-03-06 18:14:21 +0100

    [8 kyu] Days in the year

---
+++ js/solution375.js
@@ -0,0 +1,33 @@
+// A variation of determining leap years, assuming only integers are used and years can be negative and positive.
+
+// Write a function which will return the days in the year and the year entered in a string. For example:
+
+// yearDays(2000) returns "2000 has 366 days"
+// There are a few assumptions we will accept the year 0, even though there is no year 0 in the Gregorian Calendar.
+
+// Also the basic rule for validating a leap year are as follows
+
+// Most years that can be divided evenly by 4 are leap years.
+
+// Exception: Century years are NOT leap years UNLESS they can be evenly divided by 400.
+
+// So the years 0, -64 and 2016 will return 366 days. Whilst 1974, -10 and 666 will return 365 days.
+
+
+function yearDays(year)
+{
+    if(year == 0){
+        return `${year} has 366 days`
+    } else if(year%100 == 0){
+        if(year%400 == 0) {
+            return `${year} has 366 days`
+        } else {
+            return `${year} has 365 days`
+        }
+    } else {
+        return year%4 == 0 ? `${year} has 366 days` : `${year} has 365 days`
+    }
+  
+}
+
+console.log(yearDays(-64))
