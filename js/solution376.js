8debade4163e8d2c43462cfd2eb9b4c0e5f7a8bb
MateuszZaniewski <mateusz.zaniewski94@gmail.com>
2023-03-07 18:12:34 +0100

    [7 kyu] Evens and Odds

---
+++ js/solution376.js
@@ -0,0 +1,11 @@
+// This kata is about converting numbers to their binary or hexadecimal representation:
+
+// If a number is even, convert it to binary.
+// If a number is odd, convert it to hex.
+// Numbers will be positive. The hexadecimal string should be lowercased.
+
+function evensAndOdds(num){
+	return num%2 == 0 ? num.toString(2) : num.toString(16)
+}
+
+console.log(evensAndOdds(13))
