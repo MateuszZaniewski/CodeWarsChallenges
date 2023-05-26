fde80e3ee02947e532338e9413206501dbf7aa1b
MateuszZaniewski <mateusz.zaniewski94@gmail.com>
2023-05-26 06:54:47 +0200

    [7 kyu] Say hello!

---
+++ js/solution428.js
@@ -0,0 +1,16 @@
+// Write a function to greet a person. Function will take name as input and greet the person by saying hello. Return null/nil/None if input is empty string or null/nil/None.
+
+// Example:
+
+// greet("Niks") === "hello Niks!";
+// greet("") === null; // Return null if input is empty string
+// greet(null) === null; // Return null if input is null
+
+
+function greet(name) {
+    return (name == null || name == 'null' || name.length == 0) ? null : `hello ${name}!`
+}
+
+console.log(greet('Niks'))
+
+// [7 kyu] Say hello!
