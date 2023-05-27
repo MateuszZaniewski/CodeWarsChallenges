a77db4b90d58bcc319261ec96a05c80644e13c26
MateuszZaniewski <mateusz.zaniewski94@gmail.com>
2023-05-27 13:23:51 +0200

    [7 kyu] Dictionary from two lists

---
+++ js/solution429.js
@@ -0,0 +1,29 @@
+// There are two lists, possibly of different lengths. The first one consists of keys, the second one consists of values. Write a function createDict(keys, values) that returns a dictionary created from keys and values. If there are not enough values, the rest of keys should have a None (JS null)value. If there not enough keys, just ignore the rest of values.
+
+// Example 1:
+
+// keys = ['a', 'b', 'c', 'd']
+// values = [1, 2, 3]
+// createDict(keys, values) // returns {'a': 1, 'b': 2, 'c': 3, 'd': null}
+// Example 2:
+
+// keys = ['a', 'b', 'c']
+// values = [1, 2, 3, 4]
+// createDict(keys, values) // returns {'a': 1, 'b': 2, 'c': 3}
+
+
+
+function createDict(keys, values){
+    let obj = {}
+    for(let i = 0; i < keys.length; i++){
+        if(values[i] == undefined) {
+            obj[keys[i]] = null
+        } else {
+            obj[keys[i]] = values[i]
+        }
+        
+    }
+    return obj
+  }
+
+console.log(createDict(['a', 'b', 'c','d'], [1, 2, 3]))
