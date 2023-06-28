f2afae7bdfed4550e4c27261463c9c0244f3ea77
MateuszZaniewski <mateusz.zaniewski94@gmail.com>
2023-06-28 18:56:35 +0200

    [7 kyu] Find the lucky numbers

---
+++ js/solution452.js
@@ -0,0 +1,13 @@
+// Write a function filterLucky/filter_lucky() that accepts a list of integers and filters the list to only include the elements that contain the digit 7.
+
+// For example,
+
+// ghci> filterLucky [1,2,3,4,5,6,7,68,69,70,15,17]
+// [7,70,17]
+// Don't worry about bad input, you will always receive a finite list of integers.
+
+const filterLucky = (x) => {
+    return x.filter((el) => el.toString().includes(7))
+  }
+
+console.log(filterLucky([1,2,3,4,5,6,7,68,69,70,15,17]))
