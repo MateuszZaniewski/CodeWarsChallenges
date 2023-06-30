8a2e5b34b0f46f02d2d0d7ff430c29fe97edbe7e
MateuszZaniewski <mateusz.zaniewski94@gmail.com>
2023-06-30 18:32:49 +0200

    [7 kyu] Find min and max

---
+++ js/solution453.js
@@ -0,0 +1,7 @@
+// Implement a function that returns the minimal and the maximal value of a list (in this order).
+
+function getMinMax(arr){
+    return [Math.min(...arr), Math.max(...arr)]
+  };
+
+console.log(getMinMax([1,2]))
