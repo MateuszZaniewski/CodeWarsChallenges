9d14814b24659c2bcc436abd30f17d2b4be85b16
MateuszZaniewski <mateusz.zaniewski94@gmail.com>
2023-08-20 16:16:30 +0200

    [7 kyu] Filter Long Words

---
+++ js/solution479.js
@@ -0,0 +1,16 @@
+// DESCRIPTION:
+// Write a function that takes a string and an an integer n as parameters and returns a list of all words that are longer than n.
+
+// Example:
+
+// * With input "The quick brown fox jumps over the lazy dog", 4
+// * Return ['quick', 'brown', 'jumps']
+
+
+function filterLongWords(sentence, n) {
+    return sentence.split(' ').filter(function(word){
+        return word.length > n
+    })
+  }
+
+console.log(filterLongWords("The quick brown fox jumps over the lazy dog", 4))
